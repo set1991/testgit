c88cb70f4e39c2428bf1ad49a0bd1036645c4044
Alexandr Aliferenko <set1991@tut.by>
2023-12-12 09:51:22 +0300

    add memory.sh

---
+++ memory.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "memoru clear cash"
+sudo sh -c 'echo 1 >/proc/sys/vm/drop_caches'
+sudo sh -c 'echo 2 >/proc/sys/vm/drop_caches'
+sudo sh -c 'echo 3 >/proc/sys/vm/drop_caches'
+
+
