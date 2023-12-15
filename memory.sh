5eee6018b964c8c8ef724cda1e0d10d2b543a473
Alexandr Aliferenko <set1991@tut.by>
2023-12-15 15:05:45 +0300

    change memoru.sh for lesson 5

--- memory.sh
+++ memory.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-echo "memoru clear cash"
+#!/bin/bash 
+echo "$(date +"%H:%M %d/%m/%Y") Memory clear cash"
 sudo sh -c 'echo 1 >/proc/sys/vm/drop_caches'
 sudo sh -c 'echo 2 >/proc/sys/vm/drop_caches'
 sudo sh -c 'echo 3 >/proc/sys/vm/drop_caches'
